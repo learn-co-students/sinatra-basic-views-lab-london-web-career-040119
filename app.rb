3fedf13ffa401ad6352020c15f52b47158111000
ellevalentine <elle.valentine@hotmail.com>
2019-04-24 17:10:06 +0100

    done

--- app.rb
+++ app.rb
@@ -2,5 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get "/" do #a controller action that can receive and respond to a GET request to the root URL '/'
+   	erb :index # This GET request loads the index.erb file.
+   end
+
 
 end
